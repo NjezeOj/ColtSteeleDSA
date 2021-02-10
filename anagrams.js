bec7ce31f938d9d8b8cd90cf25f026bf9cfb740b
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-02-10 13:22:37 +0100

    removed method that converts strings to array

--- anagrams.js
+++ anagrams.js
@@ -6,22 +6,20 @@ function validAnagram(str1, str2){
     //convert string to lowercase
     let l_str1 = str1.toLowerCase()
     let l_str2 = str2.toLowerCase()
-    //convert string to array
-    let arr1 = [...l_str1]
-    let arr2 = [...l_str2]
     //create frequencycounter object
     let frequencyCounter1 = {}
     let frequencyCounter2 = {}
     //loop through array and create a property representing each number of the 
     //array which will have their count increased if the property already exists
-    for (let val of arr1){
+    for (let val of l_str1){
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
     }
 
-    for (let val of arr2) {
+    for (let val of l_str2) {
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
     }
 
+    //for loop which compares each key in seperate objects and
     for (let key in frequencyCounter1){
         if(!(key in frequencyCounter2)){
             return false
@@ -34,4 +32,4 @@ function validAnagram(str1, str2){
     return true;
 }
 
-console.log(validAnagram('rat','car'))
+console.log(validAnagram('rac','car'))
