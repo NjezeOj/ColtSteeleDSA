d532bded4e015dc8185204c745709c3404e9f5df
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-18 17:18:00 +0100

    Starting from 0 and General form

--- Quick Sort/pivotHelper.js
+++ Quick Sort/pivotHelper.js
@@ -2,11 +2,13 @@ const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 }
 
+////PIVOT HELPER STARTING FROM 0////////////
 function pivotHelper(arr, i = 0, end = arr.length - 1){
     
-    let pivot = 0 
+    let pivot = 0 //pivot keeps count of the indexes between start and end, 
+                //the count of numbers between start and end can also be taken note of 
     
-    for(let j = i + 1; j <= arr.length-1; j++){
+    for (let j = i + 1; j <= end; j++) {        //j = arr.length - 1
         if(arr[i] > arr[j]){   
             swap(arr, j, pivot+1) 
             pivot++ 
@@ -16,5 +18,22 @@ function pivotHelper(arr, i = 0, end = arr.length - 1){
     return pivot
 }
 
-console.log(pivotHelper([28,41,4,11,16,1,40,14,36,37,42,18]))
+console.log(pivotHelper([1,2,4,6,5,9]))
 //28,4,11,16,1,40
+
+////GENERAL FORM FOR PIVOT HELPER///////
+/* function pivotHelper(arr, i, end) {
+
+    let pivot = i
+
+    for (let j = i + 1; j <= end; j++) {        //j <= arr.length - 1
+        if (arr[i] > arr[j]) {
+            swap(arr, j, pivot + 1)
+            pivot++
+        }
+    }
+    swap(arr, pivot, i)
+    return pivot
+}
+
+console.log(pivotHelper([1, 2, 4, 6, 5, 9], 3, 5)) */
