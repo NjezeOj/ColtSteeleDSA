62e7467577268e91726f4ab81b3339d43739cad1
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-05-20 17:31:15 +0100

    Radix Helper Method

--- Radix Sort/getDigit.js
+++ Radix Sort/getDigit.js
@@ -1,6 +1,6 @@
 //First Helper needed is a function that returns a number at a particular index
 function getDigit(num, i) {
-    return Math.floor(Math.abs(num) / Math.pow(10, i)) 
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
 }
 //Math.abs to get the absolute value of an integer 
 //which converts a negative to a positive number
